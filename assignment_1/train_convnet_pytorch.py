ea45ea377fbc2bbb55563ff9996e2b8caa907bb5
RaymondKoopmanschap <raymond95@hotmail.nl>
2020-10-08 13:39:04 +0200

    Added manifold picture

--- assignment_1/train_convnet_pytorch.py
+++ assignment_1/train_convnet_pytorch.py
@@ -6,6 +6,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+sys.path.append("..")
 import argparse
 import numpy as np
 import os
